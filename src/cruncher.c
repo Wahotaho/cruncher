49df7052e2ac8b327288474c42db6ead464f5026
William <wahotaho@gmail.com>
2018-05-21 11:16:28 -0700

    first commit

---
+++ src/cruncher.c
@@ -0,0 +1,48 @@
+// Libraries included
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Global Constants
+#define len 8
+#define wid 8
+
+// Global Variables
+char * position;
+char * side_move;
+char * castling_options;
+char * en_passant_square;
+double half_move_clock;
+int full_move_number;
+char board[wid][len];
+
+// Header files
+
+#include "functions.h"
+
+int main (int argc, char * argv[]) {
+
+  // Test if argument exists
+  if (argc != 7) {
+      printf("%s\n", "Usage: ./cruncher <fen_String>");
+      exit(1);
+  }
+  //
+
+  // Initialize position components
+  position = argv[1];
+  side_move = argv[2];
+  castling_options = argv[3];
+  en_passant_square = argv[4];
+  sscanf(argv[5], "%lf", &half_move_clock);
+  sscanf(argv[6], "%d", &full_move_number);
+  //
+
+  // Load position into board
+  load_board();
+  //
+
+  print_position();
+
+  return 0;
+}
---
+++ src/functions.h
@@ -0,0 +1,72 @@
+// Function definitions
+void load_board ();
+void parse_rank (char * rank, char * pieces);
+void split (char * array[], char * buf, char * splitter);
+void print_position ();
+//
+
+void load_board () {
+  char * pos_array[len];
+  split (pos_array, position, "/");
+
+  for (int i = 0; i < len; i++) {
+    parse_rank(board[i], pos_array[i]);
+    printf("%s\n", board[i]);
+  }
+
+}
+
+void parse_rank (char * rank, char * pieces) {
+  int j = 0;
+  int num;
+
+  for (int i = 0; i < wid; i++) {
+
+      if (pieces[j] > 47 && pieces[j] < 58) {
+
+        num = pieces[j] - 48;
+
+        while (num > 0) {
+          rank[i] = '_';
+          i++;
+          num--;
+        }
+
+      } else if (pieces[j] == 'K' || pieces[j] == 'Q' || pieces[j] == 'B' || pieces[j] == 'N' || pieces[j] == 'R' || pieces[j] == 'P' || pieces[j] == 'k' || pieces[j] == 'q'
+      || pieces[j] == 'b' || pieces[j] == 'n' || pieces[j] == 'r' || pieces[j] == 'p') {
+
+        rank[i] = pieces[j];
+
+      } else {
+        printf("%s\n", "Invalid position.");
+        exit(1);
+        }
+
+        j++;
+  }
+
+}
+
+void split (char * array[], char * buf, char * splitter) {
+  int i = 0;
+  char * p = strtok (buf, splitter);
+
+  while (p != NULL) {
+      array[i++] = p;
+      p = strtok (NULL, splitter);
+  }
+}
+
+void print_position () {
+  printf(" ");
+  for (int i = 0; i < wid * 2 - 1; i++) printf("_");
+  printf("\n");
+  for (int i = 0; i < len; i++) {
+    printf("%s", "|");
+    for (int j = 0; j < wid; j++) {
+      printf("%c|", board[i][j]);
+    }
+    printf("\n");
+  }
+
+}

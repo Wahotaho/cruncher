c37647d9571c1e377cc5f2fb61b50d5ca35d28b1
William <wahotaho@gmail.com>
2018-08-08 22:25:36 -0700

    Finished tree generation

--- src/calculate.h
+++ src/calculate.h
@@ -1,6 +1,6 @@
 void go_infinite (struct position p) {
   root = create_node(p);
-  int horizon = 2;
+  int horizon = 4;
 
   heads[0] = root;
   struct node * n;
@@ -15,7 +15,7 @@ void go_infinite (struct position p) {
     }
   }
 
-  n = heads[2];
+  n = heads[4];
   i = 1;
   while (n != NULL) {
     printf("%d\n", i);
@@ -35,22 +35,17 @@ void create_child_nodes (struct node  * n) {
 
   if (heads[n -> children[0] -> depth] == NULL) {
     insertHead(&heads[n -> children[0] -> depth],n -> children[0]);
-  }
+  } else insertTail (&heads[n -> children[0] -> depth], n -> children[0]);
 
-  //printf("%d\n", n->depth);
-  //print_position(&n-> pos);
-  printf("%d\n", n -> children[0]->depth);
-  print_position(&n -> children[0]->pos);
-  printf("Num moves: %d\n", n -> pos.num_moves);
 
   for (int i = 1; i < n -> pos.num_moves; i++) {
     n -> children[i] = create_node(n->pos);
     n -> children[i] -> ancestor = n;
     n -> children[i] -> depth = n -> children[0] -> depth;
     make_move(&n -> children[i]->pos, &n -> children[i]->pos.moves[i]);
-    insertTail (&heads[n -> children[i] -> depth], n -> children[i]);
-    printf("%d\n", n -> children[i]->depth);
-    print_position(&n -> children[i]->pos);
+    n -> children[i - 1] -> neighbor = n -> children[i];
+    //insertTail (&heads[n -> children[i] -> depth], n -> children[i]);
   }
+
 
 }
